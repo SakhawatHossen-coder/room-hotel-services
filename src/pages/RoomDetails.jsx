7528d13c94888c6e37132a1d0ee54917e168ee66
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-05-25 11:32:34 +0600

    modal added on booking

--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -4,11 +4,13 @@ import {
   CardHeader,
   CardBody,
   Typography,
+  CardFooter,
   Button,
 } from "@material-tailwind/react";
 import { useParams } from "react-router-dom";
 import useAxios from "../hooks/useAxios";
 import { useQuery } from "@tanstack/react-query";
+import ModalForm from "../components/ModalForm";
 const RoomDetails = () => {
   const { id } = useParams();
   const axiosCommon = useAxios();
@@ -19,53 +21,69 @@ const RoomDetails = () => {
       return data;
     },
   });
-  console.log(Boolean(room["Special Offers"]));
-  console.log("first");
+  //   console.log(Boolean(room["Special Offers"]));
+  //   console.log("first");
   //   Boolean();
   return (
-    <Card className="w-full max-w-[55rem] mx-auto my-10 flex-row">
-      <CardHeader
-        shadow={false}
-        floated={false}
-        className="m-0 w-2/5 shrink-0 rounded-r-none"
-      >
-        <img
-          src={room["Room Images"]}
-          alt="card-image"
-          className="h-full w-full object-cover"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h6" color="gray" className="mb-4 uppercase">
-          {/* Special Offers: {room["Special Offers"] || "No Offer Available"} */}
-          Special Offers:{" "}
-          {room["Special Offers"]?.length > 0
-            ? room["Special Offers"].join(", ")
-            : "No Offer Available"}
-        </Typography>
-        {/* <Typography variant="h4" color="blue-gray" className="mb-2">
+    <>
+      {/* modal  */}
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+      {/*  */}
+      <Card className="flex flex-row max-w-[55rem] mx-auto my-10">
+        <CardHeader
+          shadow={false}
+          floated={false}
+          className="m-0 w-2/5 shrink-1 rounded-r-none"
+        >
+          <img
+            src={room["Room Images"]}
+            alt="card-image"
+            className="h-full w-full object-cover"
+          />
+        </CardHeader>
+        <CardBody>
+          <Typography variant="h6" color="gray" className="mb-4 uppercase">
+            {/* Special Offers: {room["Special Offers"] || "No Offer Available"} */}
+            Special Offers:{" "}
+            {room["Special Offers"]?.length > 0
+              ? room["Special Offers"].join(", ")
+              : "No Offer Available"}
+          </Typography>
+          {/* <Typography variant="h4" color="blue-gray" className="mb-2">
           Lyft launching cross-platform service this week
         </Typography> */}
-        <Typography color="gray" className="mb-8 font-normal">
-          {room["Room Description"]}
-        </Typography>
-        <a href="#" className="inline-block">
-          <Button variant="text" className="flex items-center gap-2">
-            Price Per Night : {room["Price per Night"]}
-          </Button>
-        </a>
-        <a href="#" className="inline-block">
-          <Button variant="text" className="flex items-center gap-2">
-            Availability : {room["Availability"] && "Available"}
-          </Button>
-        </a>
-        <a href="#" className="inline-block">
-          <Button variant="text" className="flex items-center gap-2">
-            Room Size: {room["Room Size"]}
-          </Button>
-        </a>
-      </CardBody>
-    </Card>
+          <Typography color="gray" className="mb-8 font-normal">
+            {room["Room Description"]}
+          </Typography>
+          <a className="inline-block">
+            <Button color="deep-purple" className="flex items-center gap-2">
+              Price Per Night : {room["Price per Night"]}
+            </Button>
+          </a>
+          <a className="inline-block mx-5">
+            <Button color="green" className="flex items-center gap-2">
+              Availability : {room["Availability"] && "Available"}
+            </Button>
+          </a>
+          <a className="inline-block my-2">
+            <Button color="amber" className="flex items-center gap-2">
+              Room Size: {room["Room Size"]}
+            </Button>
+          </a>
+        </CardBody>
+      </Card>
+      <CardFooter className="pt-0 flex justify-center items-center">
+        <Button
+          color="blue"
+          size="lg"
+          onClick={() => document.getElementById("my_modal_5").showModal()}
+        >
+          Book Now
+          <ModalForm />
+        </Button>
+      </CardFooter>
+    </>
   );
 };
 
